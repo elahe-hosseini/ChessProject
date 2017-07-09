9d7cc468abfc35e627bad2e606ac3304d7223a2e
elahe-hosseini <elih0294@gmail.com>
2017-07-09 12:09:23 +0430

    Add files via upload

---
+++ src/InputHandlerThread.java
@@ -0,0 +1,36 @@
+
+
+import java.io.*;
+
+class InputHandlerThread extends Thread {
+    private BufferedReader in;
+    ChessServerConnection srvcon;
+    boolean done;
+
+    public InputHandlerThread(ChessServerConnection paramChessServerConnection, BufferedReader paramBufferedReader) {
+        this.srvcon = paramChessServerConnection;
+        this.done = false;
+        this.in = paramBufferedReader;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!this.done) {
+                String str;
+                if((str = this.in.readLine()) == null){
+                    this.done = true;
+                } else {
+                    this.srvcon.reply(str);
+                }
+                if(!this.done){
+                    System.out.println("Received board data from server : " + str);
+                }
+            }
+
+        }
+        catch (IOException localException){
+            System.err.println(localException);
+        }
+    }
+}
